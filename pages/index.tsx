002c21e2a4241184fbc52059a3e9ff5fd5231ced
Jay Joo <jj534@cornell.edu>
2022-07-09 01:09:27 -0400

    Add NavHeader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
 import MobileContainer from "components/glue/MobileContainer"
+import NavHeader from "components/glue/NavHeader"
 import React from "react"
+import AlignHorizontalLeftOutlinedIcon from "@mui/icons-material/AlignHorizontalLeftOutlined"
+import InsightsOutlinedIcon from "@mui/icons-material/InsightsOutlined"
 
 const Index = () => {
-  return <MobileContainer>Index</MobileContainer>
+  const navs = [
+    {
+      label: "Entries",
+      route: "/",
+      icon: <AlignHorizontalLeftOutlinedIcon />,
+    },
+    {
+      label: "Overview",
+      route: "/overview",
+      icon: <InsightsOutlinedIcon />,
+    },
+  ]
+  return (
+    <MobileContainer>
+      <NavHeader navs={navs} />
+    </MobileContainer>
+  )
 }
 
 export default Index
