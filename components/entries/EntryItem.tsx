e4e38e04628c1aff0a113a98dbb24914c3d83b66
Jay Joo <jj534@cornell.edu>
2022-07-09 11:03:24 -0400

    Create card

--- components/entries/EntryItem.tsx
+++ components/entries/EntryItem.tsx
@@ -1,9 +1,16 @@
+import { Card, Text } from "@mantine/core"
 import React from "react"
 
 interface IEntryItemProps {}
 
 const EntryItem = ({}: IEntryItemProps) => {
-  return <div>EntryItem</div>
+  return (
+    <Card>
+      <Text size="sm" weight={500} color="text">
+        Time Log
+      </Text>
+    </Card>
+  )
 }
 
 export default EntryItem
